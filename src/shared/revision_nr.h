9550369141d4aa988a8142e1c501f4d691dc5d5f
Vinolentus <Vinolentus@getmangos.com>
2011-09-15 13:09:03 +0200

    [z1806] Avoid happines overflow in HandlePetAbandon :)
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>
    
    (based on commit cc294e2)

--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,8 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
+<<<<<<< HEAD
  #define REVISION_NR "1805"
+=======
+ #define REVISION_NR "1363"
+>>>>>>> 059d119... [s1363] Avoid happines overflow in HandlePetAbandon :)
 #endif // __REVISION_NR_H__
