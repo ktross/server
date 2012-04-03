0981274c8829d653c0ecd29bd08e3e2dafb7b915
Schmoozerd <schmoozerd@scriptdev2.com>
2012-04-03 17:02:25 +0200

    [z1893] Replace use of HashMapFolder.Find<Player> with ObjectAccessor::FindPlayer wrapper
    Signed-off-by: Nighoo <nighoo@gmail.com>

--- src/game/GroupHandler.cpp
+++ src/game/GroupHandler.cpp
@@ -772,7 +772,7 @@ void WorldSession::HandleRequestPartyMemberStatsOpcode(WorldPacket& recv_data)
     ObjectGuid guid;
     recv_data >> guid;
 
-    Player* player = HashMapHolder<Player>::Find(guid);
+    Player* player = ObjectAccessor::FindPlayer(guid, false);
     if (!player)
     {
         WorldPacket data(SMSG_PARTY_MEMBER_STATS_FULL, 3 + 4 + 2);
