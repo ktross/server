c9ab771f3fc7a5de72a0848af3e3e98c20042b97
Salja <salja2012@hotmail.de>
2012-12-28 02:30:35 +0100

    [z1829] Receiving whispers while being dnd
    Signed-off-by: Schmoozerd <schmoozerd@scriptdev2.com>
    
    (based on commit 8d2a551)

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,6 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_z1794_s1350_09_characters_mail"
- #define REVISION_DB_MANGOS "required_z1800_s1356_02_mangos_mangos_string"
+ #define REVISION_DB_MANGOS "required_z1829_s1399_01_mangos_mangos_string"
  #define REVISION_DB_REALMD "required_10008_01_realmd_realmd_db_version"
 #endif // __REVISION_SQL_H__
